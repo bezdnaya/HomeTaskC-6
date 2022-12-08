600f00c273df218f4870e5e6463a1f23f39fd956
Irina <bezdnaya@mail.ru>
2022-12-08 11:50:40 +0200

    Задача 43 количество введённых положительных чисел

---
+++ Task43/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine ("Введите количество чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < M; i++)
+{
+        Console.WriteLine ($"Ведите {i+1} число");
+        int n = Convert.ToInt32(Console.ReadLine());
+        if (n >= 0)
+            count +=1;
+}
+Console.WriteLine($"Введено {count} положительных 5числа(ел)");
