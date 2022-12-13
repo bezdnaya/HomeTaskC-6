ea0e375919d23f06a6fb681b463f820b06220c46
Irina <bezdnaya@mail.ru>
2022-12-13 15:46:51 +0200

    Точка пересечения двух прямых

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,14 +1,20 @@
-﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-// 0, 7, 8, -2, -2 -> 2
-// 1, -7, 567, 89, 223-> 3
-Console.WriteLine ("Введите количество чисел: ");
-int M = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-for (int i = 0; i < M; i++)
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine ("Введите значения k1, b1, k2 и b2 через пробел или запятую");
+string [] str = Console.ReadLine().Split(new char[]{' ',',','.'}, StringSplitOptions.RemoveEmptyEntries);
+int [] nums = str.Select(Int32.Parse).ToArray();
+if (nums[0] - nums[2] == 0)
 {
-        Console.WriteLine ($"Ведите {i+1} число");
-        int n = Convert.ToInt32(Console.ReadLine());
-        if (n > 0)
-            count +=1;
+    if (nums[1]-nums[3] == 0)
+        Console.WriteLine("Прямые совпадают и имеют множество решений");
+    else
+        Console.WriteLine("Прямые параллельны и не имеют точек пересечений");
 }
-Console.WriteLine($"Введено {count} числа(ел) больше 0");
+else
+{
+    double x = (nums[3]-nums[1])/(nums[0]-nums[2]);
+    double y = nums[0]*x+nums[1];
+    Console.WriteLine($"Координаты точки пересечения прямых {x} и {y}");
+}
+
